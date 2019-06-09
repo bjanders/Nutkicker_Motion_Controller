1cfa8b7e54c00a9c8dd87f8a5a1ac34a442c7ff2
Dirt-e <Dirt-e@users.noreply.github.com>
2019-06-09 20:09:58 +0200

    Motion Tuning Panel created

--- Nutkicker Motion Control Software/Assets/Scripts/UI/Visibility.cs
+++ Nutkicker Motion Control Software/Assets/Scripts/UI/Visibility.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[ExecuteInEditMode]
 public class Visibility : MonoBehaviour
 {
     [SerializeField] private bool visible = true;
