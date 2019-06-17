4b2d3279731777995cc49333415b5abb67e19237
Dirt-e <Dirt-e@users.noreply.github.com>
2019-06-17 14:05:44 +0200

    ForceView implemented

--- Nutkicker Motion Control Software/Assets/Scripts/BarGraph.cs
+++ Nutkicker Motion Control Software/Assets/Scripts/BarGraph.cs
@@ -9,6 +9,8 @@ public class BarGraph : MonoBehaviour
     [SerializeField] RectTransform rectTransform;
     [Header("Source")]
     [SerializeField] Stream InStream;
+    [SerializeField] bool Active;
+    [SerializeField] bool Invert;
     [SerializeField] float value;
     [Header("Layout")]
     [SerializeField] Orientation orientation;
@@ -24,13 +26,32 @@ public class BarGraph : MonoBehaviour
 
     private void Update()
     {
-        
+        if (Active)
+        {
+            value = InStream.Youngest.Datavalue;
 
-        //length = InStream.Youngest.Datavalue;
+            if (Invert)
+            {
+                value *= -1;
+            }
+        }
+
+        
 
         switch (orientation)
         {
             case Orientation.Vertical:
+                if (value > 0)
+                {
+                    rectTransform.localPosition = new Vector2(posX, posY) + new Vector2(0, value * pixelsPerG);
+                    rectTransform.sizeDelta = new Vector2(thickness, value * pixelsPerG);
+                }
+                else
+                {
+                    rectTransform.localPosition = new Vector2(posX, posY);
+                    rectTransform.sizeDelta = new Vector2(thickness, -value * pixelsPerG);
+                   
+                }
                 break;
             case Orientation.Horizontal:
                 if (value > 0)
@@ -43,14 +64,11 @@ public class BarGraph : MonoBehaviour
                     rectTransform.localPosition = new Vector2(posX, posY) + new Vector2(value * pixelsPerG, 0);
                     rectTransform.sizeDelta = new Vector2(-value * pixelsPerG, thickness);
                 }
-                
                 break;
             default:
                 break;
         }
     }
-
-
 
     enum Orientation
     {
