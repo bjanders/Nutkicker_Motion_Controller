47bfd22be13add4ccb3770c68098ee9528861c57
Dirt-e <50072319+Dirt-e@users.noreply.github.com>
2019-06-20 14:06:47 +0100

    Groundwork done for Park, Pause, Neutral and Active positions

---
+++ Nutkicker Motion Control Software/Assets/Scripts/Manipulation/CenterOfRotation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//this script makes sure that the center of rotation is offset from the motion platform by exactly the offset used to create that plaform
+//in the very first place.
+
+    [ExecuteInEditMode]
+public class CenterOfRotation : MonoBehaviour
+{
+    Transformer ParentTransformer;
+    void Update()
+    {
+        ParentTransformer = transform.parent.GetComponent<Transformer>();
+
+        Vector3 ParentOffset = new Vector3( ParentTransformer.Offset_Sway,
+                                            ParentTransformer.Offset_Heave,
+                                            ParentTransformer.Offset_Surge);
+
+        transform.localPosition = -ParentOffset;
+    }
+}
---
+++ Nutkicker Motion Control Software/Assets/Scripts/Manipulation/LerpToPosition.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class LerpToPosition : MonoBehaviour
+{
+    [SerializeField] Transform StartTransform;
+    [SerializeField] Transform EndTransform;
+    [SerializeField] [Range(0.0f, 1.0f)] float Percentage;
+
+    void Update()
+    {
+        transform.position = Vector3.Lerp(StartTransform.position, EndTransform.position, Percentage);
+        transform.rotation = Quaternion.Lerp(StartTransform.rotation, EndTransform.rotation, Percentage);
+    }
+}
