a869963bc3e3ce5cfb61137fb09703ea4c920e17
Dirt-e <50072319+Dirt-e@users.noreply.github.com>
2019-06-21 21:20:37 +0400

    Soft Start/Stop implemented via UI

--- Nutkicker Motion Control Software/Assets/Scripts/Management/StartStopSwitch.cs
+++ Nutkicker Motion Control Software/Assets/Scripts/Management/StartStopSwitch.cs
@@ -16,7 +16,7 @@ public class StartStopSwitch : MonoBehaviour
 {
     [SerializeField] public Platform platform_Pause;
     [SerializeField] public Platform platform_Physical;
-    [SerializeField] public StartStopStatus status;
+    [SerializeField] public StartStopStatus SwitchStatus;
     [SerializeField] public float transitionTime = 5.0f;
 
     private Lerp2Target lerpPause;
@@ -27,33 +27,33 @@ public class StartStopSwitch : MonoBehaviour
         lerpPause = platform_Pause.GetComponent<Lerp2Target>();
         lerpPhysical = platform_Physical.GetComponent<Lerp2Target>();
 
-        status = StartStopStatus.Park;
+        SwitchStatus = StartStopStatus.Park;
     }
 
     public void OnClick_Motion2Pause()
     {
-        if (status == StartStopStatus.Motion)
+        if (SwitchStatus == StartStopStatus.Motion)
         {
             StartCoroutine(Motion2Pause(lerpPhysical));
         }
     }
     public void OnClick_Pause2Park()
     {
-        if (status == StartStopStatus.Pause)
+        if (SwitchStatus == StartStopStatus.Pause)
         {
             StartCoroutine(Pause2Park(lerpPause));
         }
     }
     public void OnClick_Park2Pause()
     {
-        if (status == StartStopStatus.Park)
+        if (SwitchStatus == StartStopStatus.Park)
         {
             StartCoroutine(Park2Pause(lerpPause));
         }
     }
     public void OnClick_Pause2Motion()
     {
-        if (status == StartStopStatus.Pause)
+        if (SwitchStatus == StartStopStatus.Pause)
         {
             StartCoroutine(Pause2Motion(lerpPhysical));
         }
@@ -62,7 +62,7 @@ public class StartStopSwitch : MonoBehaviour
     ///////////---COROUTINES---//////////////
     IEnumerator Motion2Pause(Lerp2Target lerp)
     {
-        status = StartStopStatus.Transit;
+        SwitchStatus = StartStopStatus.Transit;
 
         while (lerp.Percentage > 0)
         {
@@ -72,11 +72,11 @@ public class StartStopSwitch : MonoBehaviour
         }
         lerp.Percentage = 0;
 
-        status = StartStopStatus.Pause;
+        SwitchStatus = StartStopStatus.Pause;
     }
     IEnumerator Pause2Park(Lerp2Target lerp)
     {
-        status = StartStopStatus.Transit;
+        SwitchStatus = StartStopStatus.Transit;
 
         while (lerp.Percentage > 0)
         {
@@ -85,11 +85,11 @@ public class StartStopSwitch : MonoBehaviour
             yield return null;
         }
         lerp.Percentage = 0;
-        status = StartStopStatus.Park;
+        SwitchStatus = StartStopStatus.Park;
     }
     IEnumerator Park2Pause(Lerp2Target lerp)
     {
-        status = StartStopStatus.Transit;
+        SwitchStatus = StartStopStatus.Transit;
 
         while (lerp.Percentage < 1)
         {
@@ -98,11 +98,11 @@ public class StartStopSwitch : MonoBehaviour
             yield return null;
         }
         lerp.Percentage = 1;
-        status = StartStopStatus.Pause;
+        SwitchStatus = StartStopStatus.Pause;
     }
     IEnumerator Pause2Motion(Lerp2Target lerp)
     {
-        status = StartStopStatus.Transit;
+        SwitchStatus = StartStopStatus.Transit;
 
         while (lerp.Percentage < 1)
         {
@@ -112,7 +112,7 @@ public class StartStopSwitch : MonoBehaviour
         }
         lerp.Percentage = 1;
 
-        status = StartStopStatus.Motion;
+        SwitchStatus = StartStopStatus.Motion;
     }
 
 }
---
+++ Nutkicker Motion Control Software/Assets/Scripts/UI/MotionIndicator.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using TMPro;
+
+[ExecuteInEditMode]
+public class MotionIndicator : MonoBehaviour
+{
+    [SerializeField] public Image image;
+    [SerializeField] public TextMeshProUGUI TMP_Text;
+
+    [SerializeField] private Color Color_Motion;
+    [SerializeField] private Color Color_Pause;
+    [SerializeField] private Color Color_Park;
+    [SerializeField] private Color Color_Transit;
+
+    [SerializeField] private StartStopSwitch startstopswitch;
+
+    private void Start()
+    {
+        image = GetComponent<Image>();
+    }
+    void FixedUpdate()
+    {
+        switch (startstopswitch.SwitchStatus)
+        {
+            case StartStopStatus.Motion:
+                image.color = Color_Motion;
+                TMP_Text.text = "--> MOTION ACTIVE <--";
+                break;
+            case StartStopStatus.Pause:
+                image.color = Color_Pause;
+                TMP_Text.text = "Paused";
+                break;
+            case StartStopStatus.Park:
+                image.color = Color_Park;
+                TMP_Text.text = "Parked";
+                break;
+            case StartStopStatus.Transit:
+                image.color = Color_Transit;
+                TMP_Text.text = "In Transit";
+                break;
+            default:
+                break;
+        }
+    }
+}
