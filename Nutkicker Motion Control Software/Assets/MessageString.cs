221bef2afb968d76a394ede0ba269c3c5da12d37
Dirt-e <50072319+Dirt-e@users.noreply.github.com>
2019-06-22 13:15:08 +0400

    Show Message String in "Controller Connect" panel

---
+++ Nutkicker Motion Control Software/Assets/MessageString.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MessageString : MonoBehaviour
+{
+    [SerializeField] MessageGenerator messagegenerator;
+    [SerializeField] TMP_Text Message_String;
+
+    private void FixedUpdate()
+    {
+        Message_String.text = messagegenerator.Message;
+    }
+}
