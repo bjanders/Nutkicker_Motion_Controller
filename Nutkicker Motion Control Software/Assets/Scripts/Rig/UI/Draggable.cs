5b9e5b7dc80100073e954fdb1d91e02eab0c22bb
Dirt-e <50072319+Dirt-e@users.noreply.github.com>
2019-10-31 20:33:01 +0100

    Draggables are confined to screen space.
    Windows that use the draggable script can no longer be placed outside of the screen area.

--- Nutkicker Motion Control Software/Assets/Scripts/Rig/UI/Draggable.cs
+++ Nutkicker Motion Control Software/Assets/Scripts/Rig/UI/Draggable.cs
@@ -3,23 +3,42 @@ using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using System.Collections;
 
-public class Draggable : MonoBehaviour, IDragHandler
+[ExecuteInEditMode]
+public class Draggable : MonoBehaviour, IDragHandler, IBeginDragHandler
 {
     RectTransform transform = null;
 
-    // Use this for initialization
+    Vector2 OriginalWindowPosition;
+    Vector2 DragStartPosition;
+    Vector2 Movement;
+
     void Awake()
     {
         transform = GetComponent<RectTransform>();
     }
-
-    public void OnDrag(PointerEventData eventData)
-    {
-        transform.position += new Vector3(eventData.delta.x, eventData.delta.y);
-    }
-
     public void OnMouseDown()
     {
         transform.SetAsLastSibling();
+        OriginalWindowPosition = transform.position;        //remember well!
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        DragStartPosition = eventData.position;
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (isOnScreen(eventData))
+        {
+            Movement = eventData.position - DragStartPosition;              //The movement of the MOUSE....
+            transform.position = OriginalWindowPosition + Movement;         //...is being added to the original window position
+        }
+    }
+    bool isOnScreen (PointerEventData eventData)
+    {
+        if (eventData.position.x >= 0 && eventData.position.x <= Screen.width && eventData.position.y >= 0 && eventData.position.y <= Screen.height)
+        {
+            return true;
+        }
+        return false;
     }
 }
