48e062669acbebb31b3ce1d383c460e84f4cab10
Dirt-e <50072319+Dirt-e@users.noreply.github.com>
2019-11-02 10:58:11 +0100

    Camera follow

---
+++ Nutkicker Motion Control Software/Assets/Scripts/CameraLock.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CameraLock : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;            //the object to stay locked to.
+
+    private void FixedUpdate()
+    {
+        transform.LookAt(target);
+    }
+
+
+}
